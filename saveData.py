7b82a91645f85da4b482b0508341a7327c4af746
Carlos Ynoquio <cynoquio@varayoc.lab>
2022-10-05 10:51:41 -0500

    se remueven archivos de prueba

--- saveData.py
+++
@@ -1,8 +0,0 @@
-import procesos_info
-
-def save_data(name1,name2):
-	df_new=procesos_info.InfoProcess(name1,name2)
-	df_new_copy=df_new
-	file_name = 'test_process.xlsx'
-	df_new_copy.to_excel(file_name,index=False) 
-	print('DataFrame is written to Excel File successfully.')
